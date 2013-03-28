3d9d1e4dbd3929c500a21f9ba095bfb812e77332
Dirk Thomas <dthomas@willowgarage.com>
2013-03-27 17:42:47 -0700

    add travis config and format test

---
+++ test/test_verify.py
@@ -0,0 +1,11 @@
+import os
+
+from rosdistro.verify import verify_files_parsable
+
+FILES_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'releases'))
+
+
+def test_verify_files():
+    index_url = 'file://' + FILES_DIR + '/index.yaml'
+    verify_files_parsable(index_url)
+
